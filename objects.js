e7b7233540ee2d7676643238dafe74083bd05ee7
Dan Brimhall <dpbrimhall@hotmail.com>
2018-08-27 23:06:15 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,5 @@
+var playlist = { Metallica: 'One' } 
+
+function updatePlaylist(playlist, artistName , songTitle) {
+  return Object.assign(playlist, { Slowdive: 'Alison' }, { 'My Bloody Valentine': 'Sometimes' },  { Phil Ochs: "Here's to the State of Mississippi"})
+}
