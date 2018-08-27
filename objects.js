ffa8982e0e871c2cd81b8fdf9e177596d656aff5
Dan Brimhall <dpbrimhall@hotmail.com>
2018-08-27 23:15:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,11 @@
-var playlist = { Metallica: 'One' } 
+
 
 function updatePlaylist(playlist, artistName , songTitle) {
   return Object.assign(playlist, { Slowdive: 'Alison' }, 
   { 'My Bloody Valentine': 'Sometimes' }, 
   { 'Phil Ochs': "Here's to the State of Mississippi"})
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  return 
 }
